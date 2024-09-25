470cf57d2a2c61259c568b13d4f3037381351f25
VladSkopenko <skopirka2k17@gmail.com>
2024-09-25 12:15:49 +0300

    basic data types

--- types.ts
+++ types.ts
@@ -1,3 +1,21 @@
-const str: string = 'Hello'
+const message: string = 'Hello Typescript'
 
-console.log(str)
+const isFetching: boolean = true
+const isLoading: boolean = false
+
+const int:number = 42
+const float: number = 4.2
+conts num: number = 3e10
+
+const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13, 21]
+const numberArray: Array<number> = [1, 1, 2, 3, 5]
+
+const words: string[] = ['Hello', 'Typescripts']
+
+
+const contact: [string, number] = ['Vlad', 123123]
+
+let variable: any = 42
+variable = []
+variable = 'new value'
+
