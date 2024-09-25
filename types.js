53cd3d96674214dd704bf48de5e909e45562f6c4
VladSkopenko <skopirka2k17@gmail.com>
2024-09-25 12:24:12 +0300

    readme upd

--- types.js
+++ types.js
@@ -1,2 +1,14 @@
-var str = 'Hello';
-console.log(str);
+var message = 'Hello Typescript';
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 4.2;
+var num = 3e10;
+var numberArrayFirst = [1, 1, 2, 3, 5, 8, 13, 21];
+var numberArraySecobd = [1, 1, 2, 3, 5];
+var words = ['Hello', 'Typescripts'];
+var contact = ['Vlad', 123123];
+var variable = 42;
+variable = [];
+variable = 'new value';
+
--- types.ts
+++ types.ts
@@ -5,10 +5,10 @@ const isLoading: boolean = false
 
 const int:number = 42
 const float: number = 4.2
-conts num: number = 3e10
+const num: number = 3e10
 
-const numberArray: number[] = [1, 1, 2, 3, 5, 8, 13, 21]
-const numberArray: Array<number> = [1, 1, 2, 3, 5]
+const numberArrayFirst: number[] = [1, 1, 2, 3, 5, 8, 13, 21]
+const numberArraySecobd: Array<number> = [1, 1, 2, 3, 5]
 
 const words: string[] = ['Hello', 'Typescripts']
 
