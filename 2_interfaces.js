fb275fdc27dab2d396f888a475f22551cf107990
VladSkopenko <skopirka2k17@gmail.com>
2024-10-04 15:25:45 +0300

    add interface

--- 2_interfaces.js
+++ 2_interfaces.js
@@ -13,6 +13,30 @@ var rect2 = {
         height: 5,
     }
 };
+rect2.color = "black";
 var rect3 = {};
 var rect4 = {};
-//==============================
+var rect5 = {
+    id: "123",
+    size: {
+        width: 20,
+        height: 21
+    },
+    getArea: function () {
+        return this.size.width * this.size.height;
+    }
+};
+var Clock = /** @class */ (function () {
+    function Clock() {
+        this.time = new Date();
+    }
+    Clock.prototype.setTime = function (date) {
+        this.time = date;
+    };
+    return Clock;
+}());
+var css = {
+    border: "1px solid black",
+    marginTop: "2px",
+    borderRadius: "5px"
+};
--- 2_interfaces.ts
+++ 2_interfaces.ts
@@ -24,7 +24,51 @@ const rect2: Rect = {
   }
 }
 
+rect2.color = "black"
+
 const rect3 = {} as Rect
 const rect4 = <Rect>{}
 
-//==============================
+// ==============================
+
+interface RectWithArea extends Rect {
+  getArea: () => number
+}
+
+const rect5: RectWithArea = {
+  id: "123",
+  size: {
+  width: 20,
+  height: 21
+  },
+  getArea(): number {
+    return this.size.width * this.size.height
+    }
+}
+
+// ===========================
+
+interface IClock {
+  time: Date
+  setTime(date: Date): void
+}
+
+class Clock implements IClock{
+  time: Date = new Date()
+
+  setTime(date:Date): void {
+    this.time = date
+    }
+}
+
+// =================
+
+interface Styles {
+  [key: string]: string
+}
+
+const css: Styles = {
+  border: "1px solid black",
+  marginTop: "2px",
+  borderRadius: "5px"
+}
